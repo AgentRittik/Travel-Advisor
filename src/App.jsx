0e338906a8cd7c9df9608ddb65af005644e2da9d
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2024-02-16 01:15:38 +0530

    Completed the setup of Nabbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,107 @@
-import { useState } from 'react'
+import React,{ useState, useEffect} from 'react'
 import './App.css'
 
-function App() {
+// Miui Imports
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
 
+
+//components imports
+import Header from './components/Header/Header';
+
+
+function App() {
+  
+  const [places, setPlaces] = useState([]);
+  const[filteredPlaces, setFilteredPlaces]  = useState([]);
+  const [childClicked, setChildClicked] = useState(null); // when we click on the marker, we want to know which marker is clicked --> we are using lifting the state up
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [weatherData, setWeatherData] = useState([]);
+  
+
+  const[isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  useEffect(() => {      // to automatically get the current location of the user in the starting. so we don't need to manually change the coordinates
+      navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+          console.log({latitude});
+          console.log({longitude});
+          setCoordinates({lat: latitude , lng: longitude});
+          console.log('vishal' , coordinates.latitude);
+      });
+  }, []);//only work once at the starting of page loding
+  
+  useEffect(() => {
+      // This useEffect will run whenever coordinates change
+      console.log('Updated coordinates:', coordinates);
+    }, [coordinates]);
+
+  useEffect(() => {
+      const filteredPlaces = places.filter((place) => place.rating > rating);
+      setFilteredPlaces(filteredPlaces);
+  },[rating]);
+  
+  // useEffect(() => {
+  //     console.log("weather",weatherData);
+  //     console.log(weatherData.coord);
+  //     console.log(weatherData.weather[0]);
+  // }, [weatherData])
+  
+
+  useEffect(() => {
+      console.log('hii' , bounds.sw , bounds.ne);
+      if(bounds.sw && bounds.ne){
+          setIsLoading(true);
+          getWeatherData(coordinates.lat , coordinates.lng)
+              .then((data) => {
+                      setWeatherData(data);
+                      console.log("wweatherData",data);
+              });
+          getPlacesData(type, bounds.sw, bounds.ne)
+              .then((data) => {
+                      setPlaces(data.filter((place) => place.name && place.num_reviews >0));
+                      setFilteredPlaces([]);
+                      setIsLoading(false);
+              });
+      }    
+  }, [type ,bounds]);  // the empty array means that this useEffect will only run once at the start of application
+  //                             // if we want to change the coordinates and bounds every time we change map , we need to add them to the array
+
+  
+  const theme = createTheme()
 
   return (
     <>
+      <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Header setCoordinates ={setCoordinates}/>
+            {/* <Grid container spacing={3} styles={{ width: '100%' }}>
+                <Grid item xs={12} md={4} >
+                    <List 
+                        places={filteredPlaces.length? filteredPlaces :places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating = {rating}
+                        setRating ={setRating}
+                    />
+                </Grid>
+                <Grid item xs={12} md={8} >
+                    <Map
+                        setCoordinates={setCoordinates} 
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                        places={filteredPlaces.length? filteredPlaces :places}
+                        setChildClicked={setChildClicked}
+                        weatherData={weatherData}
+                    />
+                </Grid>
+            </Grid> */}
+        </ThemeProvider>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import React,{ useState }from 'react';
+import { Autocomplete }  from '@react-google-maps/api';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Box from '@mui/material/Box';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+import useStyles from './styles';
+
+
+
+const Header = ({setCoordinates}) => {
+    const classes = useStyles();
+    const [autocomplete, setAutocomplete] = useState(null);
+    const onLoad = (autoC) => setAutocomplete(autoC);
+    const onPlaceChanged = () => {
+        console.log("autocomplete component",autocomplete.getPlace());
+        const lat = autocomplete.getPlace().geometry.location.lat();
+        const lng = autocomplete.getPlace().geometry.location.lng();
+        setCoordinates({lat,lng});
+    }
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography varient="h5" className ={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex"> 
+                    <Typography varient="h6" className ={classes.title}>
+                        Explore new places
+                    </Typography>
+                    <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+                        <div className= {classes.search}>
+                            <div className= {classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                        </div>
+                    </Autocomplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
